3464d2272433c79698ff998973e84d11d7bd8c88
unknown <karthu.karthu1@gmail.com>
2020-04-23 15:08:31 +0530

    added post and get request handler

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/', methods = ['POST', 'GET'])
 def index():
     if request.method == "POST":
-        return "Post Method Call Success"
+        return "Post Method Call Success, {}".format(request.get_json())
     else:
         return "Get Method Call Success"
 
